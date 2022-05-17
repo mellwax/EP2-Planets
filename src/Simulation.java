ae938cd50a3da346adfef15b4aced2e1d3824ef7
carmenwalser <e12121262@student.tuwien.ac.at>
2022-05-17 08:29:40 +0200

    Simulation works

--- src/Simulation.java
+++ src/Simulation.java
@@ -16,9 +16,9 @@ public class Simulation {
     public static final double SUN_RADIUS = 696340e3; // meters
 
     // set some system parameters
-    public static final double SECTION_SIZE = 10 * AU; // the size of the square region in space
-    public static final int NUMBER_OF_BODIES = 100;
-    public static final double OVERALL_SYSTEM_MASS = 20 * SUN_MASS; // kilograms
+    public static final double SECTION_SIZE = 5 * AU; // the size of the square region in space
+    public static final int NUMBER_OF_BODIES = 1000;
+    public static final double OVERALL_SYSTEM_MASS = NUMBER_OF_BODIES * SUN_MASS; // kilograms
     public static final boolean DRAW_OCTANTS = false;
     public static final int MAX_BODIES_PER_OCTANT = 1;
 
@@ -29,27 +29,29 @@ public class Simulation {
         CodeDraw cd = new CodeDraw();
         cd.clear(Color.BLACK);
 
-        Octant octant = new Octant(0, 0, 0, 600);
+        Octant octant = new Octant(-SECTION_SIZE/2, -SECTION_SIZE/2, -SECTION_SIZE/2, SECTION_SIZE);
         Octree octree = new Octree(octant);
-        Random random = new Random(777);
+        Random random = new Random(2022);
 
         for (int i = 0; i < NUMBER_OF_BODIES; i++) {
-            Body b = new Body(random.nextDouble(600),
-                    new Vector3(random.nextDouble(600), random.nextDouble(600), random.nextDouble(600)),
-                    new Vector3(random.nextDouble(100), random.nextDouble(100), random.nextDouble(100)));
-            octree.add(b);
-            //System.out.println(b);
+
+            double mass = Math.abs(random.nextGaussian()) * OVERALL_SYSTEM_MASS / NUMBER_OF_BODIES; // kg
+
+            double massCenterX = 0.3 * random.nextGaussian() * SECTION_SIZE/2;
+            double massCenterY = 0.3 * random.nextGaussian() * SECTION_SIZE/2;
+            double massCenterZ = 0.3 * random.nextGaussian() * SECTION_SIZE/2;
+
+            double currentMovementX = 0 + random.nextGaussian() * 5e5;
+            double currentMovementY = 0 + random.nextGaussian() * 5e5;
+            double currentMovementZ = 0 + random.nextGaussian() * 5e5;
+
+            Vector3 massCenter = new Vector3(massCenterX, massCenterY, massCenterZ);
+            Vector3 currentMovement = new Vector3(currentMovementX, currentMovementY, currentMovementZ);
+
+            octree.add(new Body(mass, massCenter, currentMovement));
+
         }
-        //System.out.println(octree);
-        /*System.out.println(octree.size());
-        System.out.println(octree.mass());
-        System.out.println(octree.massCenter());
-        Body test = new Body(octree.mass(), octree.massCenter(), new Vector3());
-        cd.setColor(Color.YELLOW);
-        cd.drawText(octree.massCenter().x,octree.massCenter().y, "Octree Mass Center");
-        test.draw(cd);
-        octree.draw(cd);
-        cd.show();*/
+
 
         double seconds = 0;
 
@@ -57,16 +59,18 @@ public class Simulation {
         while (true) {
 
             seconds++; // each iteration computes the movement of the celestial bodies within one second.
-            System.out.println(seconds);
+            //System.out.println(seconds);
 
             octree.calculateForce(octree);
             octree.moveBodies();
+            //octree.printOctants();
 
             // show all movements in the canvas only every hour (to speed up the simulation)
-            if (seconds % (3600) == 0) {
+            if (seconds % (360) == 0) {
                 // clear old positions (exclude the following line if you want to draw orbits).
                 cd.clear(Color.BLACK);
                 octree.draw(cd);
+                System.out.println("Image: " + seconds/360);
                 // show new positions
                 cd.show();
             }
